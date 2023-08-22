f8bda65dc76a0c3964c1881f63ff2d5209ee0f23
Nikola <102659475+Dzekanaa@users.noreply.github.com>
2023-08-22 00:28:12 +0000

    privacy policy page(nema dark mode)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "index2.html"],
   theme: {
     extend: {
       colors: {
